adcee8a9e3ef97dc1addd9f2cf6d45cf4c0fd2b3
BBEK-Anand <bibekanandahati75@gmail.com>
2025-10-30 10:19:21 +0530

    refine

--- src/lwf/__init__.py
+++ src/lwf/__init__.py
@@ -12,15 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-PyTorchLabFlow
+LwFlow
 ==============
 
 This is your go-to offlinesolution for managingPyTorch experimentswith ease.
 Run experiments securely on your local machine, no data sharing with 3rd parties.
 Need more power? Seamlesslytrasfer your setup to a high-end system without any reconfiguration.
-Wheather you are on a laptop or a workstation, PyTorchLabFlow ensures flexibility and privacy,
+Wheather you are on a laptop or a workstation, LwFlow ensures flexibility and privacy,
 allowingyou to experiment anywhere, anytime, without internet dependency.
-[Read more at github](https://github.com/BBEK-Anand/PyTorchLabFlow)
+[Read more at github](https://github.com/ExperQuick/LwFlow)
 
 
 """
