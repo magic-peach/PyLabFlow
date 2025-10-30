a6b1ec606f96b70c29eb7349b45fa33373aff7e6
BBEK-Anand <bibekanandahati75@gmail.com>
2025-10-30 10:40:59 +0530

    refine

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='LwFlow',
-    version='0.3',
+    version='0.1',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     include_package_data=True,
